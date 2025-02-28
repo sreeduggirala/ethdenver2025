10b3ebee52b4f6f5bd23d40a1c6357a7bb9cafdc
Sree Duggirala <sreevanthduggirala@gmail.com>
2025-02-28 11:57:28 -0700

    add net worth, prices, portfolio

--- backend/moralis.py
+++ backend/moralis.py
@@ -1,6 +1,5 @@
 import aiohttp
 import asyncio
-from dexscreener import get_token_info
 from dotenv import load_dotenv
 import os
 
@@ -24,23 +23,41 @@ async def get_portfolio(address: str):
         print(f"Error fetching wallet portfolio: {e}")
         return None
     
+async def get_token_price(address: str):
+    url = f'https://solana-gateway.moralis.io/token/mainnet/{address}/price'
+
+    headers = {
+        'accept': 'application/json',
+        'X-API-Key': api_key,
+    }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                res = await response.json()
+                return float(res['usdPrice'])
+    
+    except Exception as e:
+        print(f"Error fetching token price: {e}")
+        return None
+    
 
 async def get_net_worth(address: str):
     portfolio = await get_portfolio(address)
     
-    if portfolio:
-        try:
-            net_worth = 0
-            for token in portfolio['tokens']:
-                token_info = await get_token_info('solana', token['associatedTokenAddress'])
-                print(token_info)
-                if token_info:
-                    net_worth += float(token['amount']) * float(token_info['priceUsd'])
-            # add amount of SOL * price of SOL to net_worth
-            return net_worth
-        except Exception as e:
-            print(f"Error calculating net worth: {e}")
+    try:
+        if not portfolio:
             return None
         
-print(asyncio.run(get_net_worth('5PRw4hYJJh8EjLBg4UZTNEKHSYvyjxLKy3PHBn5PYxHb')))
+        net_worth = 0
+        for token in portfolio['tokens']:
+            token_price = await get_token_price(token['mint'])
+            
+            if token_price:
+                net_worth += float(token['amount']) * float(token_price)
+        
+        return net_worth
     
+    except Exception as e:
+        print(f"Error calculating net worth: {e}")
+        return None
