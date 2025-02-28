c53cb6c1b5e19439b7ace0d26871e5904a4b8bfb
CMEONE <kabir@ramzan.me>
2025-02-28 14:53:34 -0700

    Account for prices

--- contracts/src/Fantasy.sol
+++ contracts/src/Fantasy.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.20;
 
 contract Fantasy {
-	mapping(address => string[5]) public drafts;
-
 	struct Team {
 		address[5] members;
 	}
 
+	mapping(address => string[5]) public drafts;
 	mapping(uint256 => Team) public teams;
+	mapping(string => uint256) public prices;
+	mapping(address => uint256) public balances;
 
 	event TeamCreated(uint256 id, address creator);
 	event PlayerJoined(address player, uint256 id);
@@ -25,20 +26,37 @@ contract Fantasy {
 	function join(uint256 id) public {
 		Team storage team = teams[id];
 		bool joined = false;
-		for (uint256 i = 0; i < 5; i++) {
+		for(uint256 i = 0; i < 5; i++) {
 			if (team.members[i] == address(0)) {
 				team.members[i] = msg.sender;
+				for(uint256 i = 0; i < 5; i++) {
+					draft[msg.sender][i] = "";
+				}
 				joined = true;
 				emit PlayerJoined(msg.sender, id);
 				break;
 			}
 		}
-		require(joined, "The team is full");
+		require(joined, "Team is already full");
 	}
 
 	function draft(address player, string memory kol, uint256 index) public {
 		require(index < 5, "Index must be less than 5");
+		uint256 price = 10;
+		if(prices[kol] > 0) {
+			price = prices[kol];
+		}
+		require(balances[player] >= price, "Insufficient funds");
+		balances[player] -= price;
 		drafts[player][index] = kol;
 		emit Drafted(player, kol, index);
+	}
+
+	function getMembersFromTeam(uint256 id) public view returns (address[5] memory) {
+		return teams[id].members;
+	}
+
+	function getDraft(address player) public view returns (string[5] memory) {
+		return drafts[player];
 	}
 }
